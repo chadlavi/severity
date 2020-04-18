a6a6c929fabe971c8f47b950a9c91638d96ae2e7
Chad Lavimoniere <chadlavimoniere@Chads-2020-Air.home>
2020-04-18 17:55:10 -0400

    first commit

---
+++ docs/precache-manifest.c8a81f6f9d75cce67b78182e3ee2c388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278543ca89029609f11a99715e52e24a",
+    "url": "/severity/index.html"
+  },
+  {
+    "revision": "0b8c4c227548a765afc3",
+    "url": "/severity/static/css/main.1c266bb0.chunk.css"
+  },
+  {
+    "revision": "9addf34461648597e657",
+    "url": "/severity/static/js/2.ffb39805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/severity/static/js/2.ffb39805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8c4c227548a765afc3",
+    "url": "/severity/static/js/main.5bdbc132.chunk.js"
+  },
+  {
+    "revision": "02a6d559e91bdcd344ff",
+    "url": "/severity/static/js/runtime-main.3eb36a25.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const triage = (impact: number, customers: number): 'cosmetic' | 'low' | 'medium' | 'high' | 'critical' => {
+  if (impact < 12.5) return 'cosmetic'
+  if (impact < 25 && customers < 25) return 'low'
+  if (
+    (impact < 90 && customers < 25) ||
+    (impact < 75 && customers < 50) ||
+    (impact < 50)
+  ) return 'medium'
+  if (impact < 90 && impact >=50 && customers >= 25 && (
+    (impact >= 75 || customers >= 50) &&
+    !(customers >= 75 && impact >= 75)
+  )) return 'high'
+  return 'critical'
 }
 
-export default App;
+const emoji = {
+  cosmetic: '🙃',
+  low: '🤔',
+  medium: '☹️',
+  high: '😫',
+  critical: '🥵',
+}
+
+const App = (): JSX.Element => {
+  const [impact, setImpact] = React.useState<number|undefined>()
+  const [customers, setCustomers] = React.useState<number|undefined>()
+
+  const severity = triage(impact || 0, customers || 0)
+
+  const severityString = `${severity}${!['cosmetic', 'critical'].includes(severity) && ' priority'}`
+
+  const onChange = (
+    setter: React.Dispatch<React.SetStateAction<number|undefined>>
+  ) => (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue = parseFloat(e.currentTarget.value)
+    if ((0 <= newValue && newValue <= 100) || !newValue) setter(newValue)
+  }
+
+  return (
+    <main>
+      <h1>
+        Bug prioritizer
+      </h1>
+      <form
+        autoComplete={'false'}
+      >
+        <label>
+          <span>
+            What percentage of users does this impact?
+          </span>
+          <input
+            inputMode={'decimal'}
+            max={100}
+            min={0}
+            name={'customers'}
+            onChange={onChange(setCustomers)}
+            type={'number'}
+            value={customers || ''}
+          />
+        </label>
+        <label>
+          <span>
+            On a scale of 0 to 100, how much does this inhibit an affected user&rsquo;s ability to use the app?
+          </span>
+          <input
+            inputMode={'decimal'}
+            max={100}
+            min={0}
+            name={'impact'}
+            onChange={onChange(setImpact)}
+            pattern={'[0-9.]*'}
+            type={'number'}
+            value={impact || ''}
+          />
+        </label>
+      </form>
+      {(customers && impact) ? <div className={`result ${severity}`}>
+        {emoji[severity]} Sounds like this is a <u>{severityString}</u> bug
+      </div> : ''}
+    </main>
+  )
+}
+
+export default App
